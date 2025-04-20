7af25540423442143c261f3445b286d11e251203
shobhit <shobhitrai.developer@gmail.com>
2025-04-21 03:07:55 +0530

    first commit

---
+++ src/main/java/com/sbit/chatify/ChatifyApplication.java
@@ -0,0 +1,13 @@
+package com.sbit.chatify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbit/chatify/ChatifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbit.chatify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
