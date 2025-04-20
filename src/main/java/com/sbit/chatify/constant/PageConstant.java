e54615e864751794e2d75a047ddb6d0fbef6888c
shobhit <shobhitrai.developer@gmail.com>
2025-04-21 04:17:09 +0530

    added

---
+++ src/main/java/com/sbit/chatify/constant/PageConstant.java
@@ -0,0 +1,11 @@
+package com.sbit.chatify.constant;
+
+public class PageConstant {
+    String SIGN_UP = "signup";
+    String WALL = "wall";
+    String ERROR ="error";
+    String LOGIN = "login";
+    String REDIRECT_LOGIN = "redirect:login";
+    String REDIRECT_SIGNUP = "redirect:signin";
+    String REDIRECT_WALL = "redirect:wall";
+}
