529af17e7e4df3beb887ca4909ce18e493389e08
shobhit <shobhitrai.developer@gmail.com>
2025-04-24 02:02:50 +0530

    added

--- src/main/java/com/sbit/chatify/constant/UrlConstant.java
+++ src/main/java/com/sbit/chatify/constant/UrlConstant.java
@@ -4,9 +4,6 @@ public interface UrlConstant {
     String VALIDATE_SIGNUP = "validate-signup";
     String LOGIN = "login";
     String SIGNUP = "signup";
-    String LOGOUT = "logout";
-    String FORGOT_PASSWORD = "forgot-password";
-    String RESET_PASSWORD = "reset-password";
     String SEND_FRIEND_REQUEST = "send-friend-request";
 
 }
