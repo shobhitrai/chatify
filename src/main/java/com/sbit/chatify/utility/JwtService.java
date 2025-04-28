2a897aa4bf5ef5aa1540a6dd56c592b229da792b
shobhit <shobhitrai.developer@gmail.com>
2025-04-28 20:52:50 +0530

    added

--- src/main/java/com/sbit/chatify/utility/JwtService.java
+++ src/main/java/com/sbit/chatify/utility/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
@@ -14,16 +15,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
 @Component
 public class JwtService {
-//    @Value("${security.jwt.secret-key}")
-//    private String secretKey;
-//
-//    @Value("${security.jwt.expiration-time}")
-//    private long jwtExpiration;
 
     public String extractUserId(String token) {
         return extractClaim(token, Claims::getSubject);
