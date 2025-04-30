1399c8c4b17a8069f171c296592c29bbf155fa57
shobhit <shobhitrai.developer@gmail.com>
2025-05-01 03:08:52 +0530

    added

--- src/main/java/com/sbit/chatify/service/impl/SocketServiceImpl.java
+++ src/main/java/com/sbit/chatify/service/impl/SocketServiceImpl.java
@@ -4,7 +4,6 @@ import com.sbit.chatify.service.SocketService;
 import com.sbit.chatify.websocket.SocketUtil;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
