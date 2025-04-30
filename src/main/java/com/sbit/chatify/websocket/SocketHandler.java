828d93bc67f4986ccc298dad7c526c77bc816170
shobhit <shobhitrai.developer@gmail.com>
2025-04-30 22:59:23 +0530

    added

--- src/main/java/com/sbit/chatify/websocket/SocketHandler.java
+++ src/main/java/com/sbit/chatify/websocket/SocketHandler.java
@@ -1,27 +1,19 @@
 package com.sbit.chatify.websocket;
 
 import jakarta.websocket.*;
-import jakarta.websocket.server.HandshakeRequest;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 
-import java.nio.ByteBuffer;
-import java.util.List;
-import java.util.Map;
-
-@ServerEndpoint(value = "/chat", configurator = CustomConfigurator.class)
+@ServerEndpoint("/socket/{userId}")
 public class SocketHandler {
 
     @OnMessage
-    public void onMessage(String message, Session session) {
+    public void onMessage(@PathParam("userId") String userId, String message, Session session) {
 
     }
 
     @OnOpen
-    public void onOpen(Session session, EndpointConfig conf) {
-        HandshakeRequest req = (HandshakeRequest) conf.getUserProperties().get("handshakereq");
-        Map<String, List<String>> headers = req.getHeaders();
-        String userId = "yututto";
+    public void onOpen(@PathParam("userId") String userId, Session session) {
         SocketUtil.register(userId, session);
     }
 
@@ -33,11 +25,6 @@ public class SocketHandler {
     @OnError
     public void onError(@PathParam("userId") String userId, Session session, Throwable throwable) {
         SocketUtil.transportError(userId, session, throwable);
-    }
-
-    @OnMessage
-    public void binaryMessage(Session session, ByteBuffer msg) {
-        System.out.println("Binary message: " + msg.toString());
     }
 
 
